168c777d0102786c8cdcb78ca4c106416cf203f5
DrewnowskiBartlomiej <20323@ecam.be>
2023-01-30 17:34:35 +0100

    add max val

--- stream_record.py
+++ stream_record.py
@@ -5,12 +5,21 @@ import numpy as np
 from scipy.signal import butter,filtfilt,detrend,iirnotch,welch
 from time import sleep,time
 
+
 #osc import
 from pythonosc import udp_client
 from pythonosc import osc_message_builder
 
+from utils import *
+
 # Create UDP client
 client = udp_client.UDPClient('192.168.1.255',9998,  True)
+
+current_delta_max = 0.0
+current_theta_max = 0.0
+current_alpha_max = 0.0
+current_beta_max = 0.0
+current_gamma_max = 0.0
 
 def streamCleanData():
 
@@ -33,27 +42,42 @@ class StreamCleanDataOSC():
         """Init"""
         self.stream = stream
         self.dejitter = dejitter
-        self.fs_speed = 128
-        # self.window = 5
+        self.fs_speed = 32
         self.inlet = StreamInlet(stream, max_chunklen=256)
-        # info = self.inlet.info()
+        info = self.inlet.info()
         # self.sfreq = info.nominal_srate()
-        # self.n_chan = info.channel_count()
-        self.display_every = 2
+        self.n_chan = info.channel_count()
+        self.preprocess_every = int(256/self.fs_speed)
+        self.data = np.zeros((256, self.n_chan))
+
+        self.connection_quality_channels = [3,3,3,3]
+
 
     def update_OSCstream(self):
-        k = 0
+        nb_pull = 0
+        signal_clean_TP9,signal_clean_AF7,signal_clean_AF8,signal_clean_TP10 = [[],[],[],[]]
         try:
             while self.started:
 
                 samples, timestamps = self.inlet.pull_chunk(timeout=1.0, max_samples=self.fs_speed)#256
+                
                 if timestamps:
-                    self.process(samples)
-                    t1 = time()
-                    print(t1)
-                    k = 1
-                if k == self.display_every:
-                    k = 0
+                    self.data = np.vstack([self.data, samples])
+                    self.data = self.data[-256:]
+                    [signal_clean_TP9,signal_clean_AF7,signal_clean_AF8,signal_clean_TP10] = self.preprocess(self.data)
+                    self.process(signal_clean_TP9,signal_clean_AF7,signal_clean_AF8,signal_clean_TP10)
+
+                    #MODIF
+                    record(np.column_stack([signal_clean_TP9,signal_clean_AF7,signal_clean_AF8,signal_clean_TP10]),timestamps)
+                    # ValueError: all the input array dimensions for the concatenation axis must match exactly, but along dimension 0, the array at index 0 has size 256 and the array at index 3 has size 0
+                    nb_pull +=1
+                    if nb_pull == self.preprocess_every:
+                        print('Full message')
+                        # [signal_clean_TP9,signal_clean_AF7,signal_clean_AF8,signal_clean_TP10] = self.preprocess(self.data)
+                        nb_pull = 0
+                    else:
+                        sleep(0.08)
+                
 
         except RuntimeError as e:
             raise
@@ -78,11 +102,9 @@ class StreamCleanDataOSC():
         signal = signal - np.mean(signal)
 
         # filtering signal with Butterworth passband filter 0.1 - 50Hz
-        # fs = 256 # Sample frequency (Hz)
-        fs = self.fs_speed
+        fs = 256 # Sample frequency (Hz)
         nyq = fs/2
         order = 4
-        # Wn = [0.1/nyq, 50.0/nyq]
         b,a = butter(order,[0.1/nyq,50.0/nyq],btype='band')
         signal = filtfilt(b,a,signal)
         
@@ -117,8 +139,8 @@ class StreamCleanDataOSC():
     def frequency_bands_separation(self, signal_clean):
         # Frequency analysis of the signal. Returns the bandpowers of the 5
         # main frequency bands (delta, theta, alpha, beta, gamma)
-        # fs = 256
-        fs = self.fs_speed
+        fs = 256
+        # fs = 32
         # calcule de power spectral density by Welch method
         f,pxx = welch(signal_clean,fs)
         # Separation of PSD in bandpower
@@ -132,42 +154,67 @@ class StreamCleanDataOSC():
 
 
 
-    def process(self, matrix_data):
+    def preprocess(self, matrix_data):
         # Data preparation
-        data = np.reshape(matrix_data, (self.fs_speed, 4)) # bcz 5 different channel of data (change 4 -> 5 like in OpenVibe) 4 channels for csv files from Louise
+        data = np.reshape(matrix_data, (256, 4)) # bcz 5 different channel of data (change 4 -> 5 like in OpenVibe) 4 channels for csv files from Louise
         TP9_data = data[:,0]
         AF7_data = data[:,1]
         AF8_data = data[:,2]
         TP10_data = data[:,3]
 
         # Create a list of connection quality of each channel
-        connection_quality_channels = [self.connectionQuality(TP9_data),self.connectionQuality(AF7_data),self.connectionQuality(AF8_data),self.connectionQuality(TP10_data)]
+        self.connection_quality_channels = [self.connectionQuality(TP9_data),self.connectionQuality(AF7_data),self.connectionQuality(AF8_data),self.connectionQuality(TP10_data)]
         # Check if at least one sensor is good
+
+        if self.connection_quality_channels[0] < 3: # TP9
+            signal_clean_TP9 = self.preprocessing(TP9_data,False) # Preprocessing
+        else:
+            # signal_clean_TP9 = np.zeros(256)
+            signal_clean_TP9 = []
+
+        if self.connection_quality_channels[1] < 3: # AF7
+            signal_clean_AF7 = self.preprocessing(AF7_data,False) # Preprocessing
+        else:
+            # signal_clean_AF7 = np.zeros(256)
+            signal_clean_AF7 = []
+
+        if self.connection_quality_channels[2] < 3: # AF8
+            signal_clean_AF8 = self.preprocessing(AF8_data,False) # Preprocessing
+        else:
+            # signal_clean_AF8 = np.zeros(256)
+            signal_clean_AF8 = []
+
+        if self.connection_quality_channels[3] < 3: # TP10
+            signal_clean_TP10 = self.preprocessing(TP10_data,False) # Preprocessing
+        else:
+            # signal_clean_TP10 = np.zeros(256)
+            signal_clean_TP10 = []
+        
+        return signal_clean_TP9,signal_clean_AF7,signal_clean_AF8,signal_clean_TP10
+
+    def process(self,signal_clean_TP9,signal_clean_AF7,signal_clean_AF8,signal_clean_TP10):
+
         nb_good_sensors = 0
-        if connection_quality_channels[0] < 3: # TP9
-            signal_clean = self.preprocessing(TP9_data,False) # Preprocessing
-            [deltaTP9, thetaTP9, alphaTP9, betaTP9, gammaTP9] = self.frequency_bands_separation(signal_clean)# frequency bands separation
+        if signal_clean_TP9 != []: # TP9
+            [deltaTP9, thetaTP9, alphaTP9, betaTP9, gammaTP9] = self.frequency_bands_separation(signal_clean_TP9)# frequency bands separation
             nb_good_sensors += 1
         else:
             [deltaTP9, thetaTP9, alphaTP9, betaTP9, gammaTP9] = [0,0,0,0,0]
 
-        if connection_quality_channels[1] < 3: # AF7
-            signal_clean = self.preprocessing(AF7_data,False) # Preprocessing
-            [deltaAF7, thetaAF7, alphaAF7, betaAF7, gammaAF7] = self.frequency_bands_separation(signal_clean)# frequency bands separation
+        if signal_clean_AF7 != []: # AF7
+            [deltaAF7, thetaAF7, alphaAF7, betaAF7, gammaAF7] = self.frequency_bands_separation(signal_clean_AF7)# frequency bands separation
             nb_good_sensors += 1
         else:
             [deltaAF7, thetaAF7, alphaAF7, betaAF7, gammaAF7] = [0,0,0,0,0]
 
-        if connection_quality_channels[2] < 3: # AF8
-            signal_clean = self.preprocessing(AF8_data,False) # Preprocessing
-            [deltaAF8, thetaAF8, alphaAF8, betaAF8, gammaAF8] = self.frequency_bands_separation(signal_clean)# frequency bands separation
+        if signal_clean_AF8 != []: # AF8
+            [deltaAF8, thetaAF8, alphaAF8, betaAF8, gammaAF8] = self.frequency_bands_separation(signal_clean_AF8)# frequency bands separation
             nb_good_sensors += 1
         else:
             [deltaAF8, thetaAF8, alphaAF8, betaAF8, gammaAF8] = [0,0,0,0,0]
 
-        if connection_quality_channels[3] < 3: # TP10
-            signal_clean = self.preprocessing(TP10_data,False) # Preprocessing
-            [deltaTP10, thetaTP10, alphaTP10, betaTP10, gammaTP10] = self.frequency_bands_separation(signal_clean)# frequency bands separation
+        if signal_clean_TP10 != []: # TP10
+            [deltaTP10, thetaTP10, alphaTP10, betaTP10, gammaTP10] = self.frequency_bands_separation(signal_clean_TP10)# frequency bands separation
             nb_good_sensors += 1
         else:
             [deltaTP10, thetaTP10, alphaTP10, betaTP10, gammaTP10] = [0,0,0,0,0]
@@ -180,11 +227,23 @@ class StreamCleanDataOSC():
             gamma = (gammaTP9+gammaAF7+gammaAF8+gammaTP10)/nb_good_sensors
         else:
             delta, theta, alpha, beta, gamma = 0,0,0,0,0
+        
+        # find the maximum
+        global current_delta_max
+        global current_theta_max 
+        global current_alpha_max
+        global current_beta_max 
+        global current_gamma_max
+        
+        current_delta_max = highest_value(delta, current_delta_max)
+        current_theta_max = highest_value(theta, current_theta_max)
+        current_alpha_max = highest_value(alpha, current_alpha_max)
+        current_beta_max = highest_value(beta, current_beta_max)
+        current_gamma_max = highest_value(gamma, current_gamma_max)
+
 
         # Send data on UDP port (protocol OSC)
         # print ([delta,theta,alpha,beta,gamma])
-
-        # bundle = osc_bundle_builder.OscBundleBuilder(osc_bundle_builder.IMMEDIATELY)
         message_array = osc_message_builder.OscMessageBuilder(address = '/mean')
         message_array2 = osc_message_builder.OscMessageBuilder(address = '/allwaves')
 
@@ -193,9 +252,12 @@ class StreamCleanDataOSC():
         message_array.add_arg(alpha)
         message_array.add_arg(beta)
         message_array.add_arg(gamma)
-        for i in connection_quality_channels:
+
+        for i in self.connection_quality_channels:
             message_array.add_arg(i)
-        
+        for i in [current_delta_max, current_theta_max, current_alpha_max, current_beta_max, current_gamma_max]:
+            message_array.add_arg(i)
+
         for i in [deltaTP9, thetaTP9, alphaTP9, betaTP9, gammaTP9]:
             message_array2.add_arg(i)
         for i in [deltaAF7, thetaAF7, alphaAF7, betaAF7, gammaAF7]:
@@ -209,6 +271,9 @@ class StreamCleanDataOSC():
         message_array2.build()
         client.send(message_array.build())
         client.send(message_array2.build())
+        # print(time())
+
+
 
 # Run teh stream
-# streamCleanData()
+streamCleanData()
---
+++ utils.py
@@ -0,0 +1,84 @@
+
+
+from ast import List
+import os
+from pathlib import Path
+from time import gmtime, strftime
+import time
+import numpy as np
+import pandas as pd
+from typing import Union, List, Optional
+from sklearn.linear_model import LinearRegression
+
+def highest_value(input, current_max):
+    if input > current_max:
+        current_max = input
+    # print(current_max)
+    return current_max
+
+# def record(signal_clean_TP9,signal_clean_AF7,signal_clean_AF8,signal_clean_TP10):
+#     filename = os.path.join(os.getcwd(), ("C:/Users/drbar/OneDrive/Pulpit/Stage 2022/Recording/recording_%s.csv" % strftime("%Y-%m-%d-%H.%M.%S", gmtime())))
+
+def record(data, timestamp, duration: int = 20,continuous: bool = True,) -> None:
+
+    filename = os.path.join(os.getcwd(), "C:/Users/drbar/OneDrive/Pulpit/Stage 2022/Recording/EEG_recording_%s.csv" %(strftime('%Y-%m-%d-%H.%M.%S', gmtime())))
+
+
+    res = []
+    timestamps = []
+    t_init = time()
+    last_written_timestamp = None
+    print('Start recording at time t=%.3f' % t_init)
+    # data, timestamp = inlet.pull_chunk(timeout=1.0, max_samples=chunk_length)
+
+    res.append(data)
+    timestamps.extend(timestamp)
+
+    # Save every 5s
+    if continuous and (last_written_timestamp is None or last_written_timestamp + 5 < timestamps[-1]):
+        _save(
+            filename,
+            res,
+            timestamps,
+            last_written_timestamp=last_written_timestamp,
+        )
+        last_written_timestamp = timestamps[-1]
+
+        
+
+    _save(
+        filename,
+        res,
+        timestamps,
+    )
+
+    print("Done - wrote file: {}".format(filename))
+
+
+def _save(
+    filename: Union[str, Path],
+    res: list,
+    timestamps: list,
+    last_written_timestamp: Optional[float] = None,
+):
+
+    res = np.concatenate(res, axis=0)
+    timestamps = np.array(timestamps)
+
+    res = np.c_[timestamps, res]
+    data = pd.DataFrame(data=res, columns=["timestamps","TP9","AF7","AF8","TP10"])
+
+    directory = os.path.dirname(filename)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # If file doesn't exist, create with headers
+    # If it does exist, just append new rows
+    if not Path(filename).exists():
+        # print("Saving whole file")
+        data.to_csv(filename, float_format='%.3f', index=False)
+    else:
+        # print("Appending file")
+        # truncate already written timestamps
+        data = data[data['timestamps'] > last_written_timestamp]
+        data.to_csv(filename, float_format='%.3f', index=False, mode='a', header=False)
